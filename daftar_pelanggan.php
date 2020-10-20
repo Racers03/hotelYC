c151363061ccd18ba30eed8040ee908625bd9cc1
Cheah Zixu <cheah3838@gmail.com>
2020-10-20 18:00:42 +0800

    Fixed indentation and errors

--- daftar_pelanggan.php
+++ daftar_pelanggan.php
@@ -1,10 +1,10 @@
-<?php 
-//sambung ke pangkalan data
-require('config.php');
-//sambung ke fail header
-require('header.php');
-//semak sama ada data dengan IC Pelanggan telah dihantar
-if (isset($_POST['idpelanggan'])) {
+<?php
+  //sambung ke pangkalan data
+  require('config.php');
+  //sambung ke fail header
+  require('header.php');
+  //semak sama ada data dengan IC Pelanggan telah dihantar
+  if (isset($_POST['idpelanggan'])) {
     //pembolehubah untuk memegang data yang dihantar
     $id = $_POST['idpelanggan'];
     $nama = $_POST['nama'];
@@ -14,73 +14,50 @@ if (isset($_POST['idpelanggan'])) {
     $poskod = $_POST['poskod'];
     $bandar = $_POST['bandar'];
     $negeri = $_POST['negeri'];
-//Tambah rekod baru ke dalam table pelanggan
-$sql = "INSERT INTO pelanggan (idpelanggan,nama,nomhp)
-VALUES ('$id','$nama','$hp')";
-$hasil=mysqli_query($samb,$sql);
-//Tambah rekod baru ke dalam table alamat
-$sql1 = "INSERT INTO alamat (idalamat,alamat1,alamat2,bandar, poskod, 
-negeri,idpelanggan)
-VALUES (NULL,'$alamat1','$alamat2','$poskod','$bandar','$negeri','$id')";
-$hasil=mysqli_query($samb,$sql1);
+    //Tambah rekod baru ke dalam table pelanggan
+    $sql = "INSERT INTO pelanggan (idpelanggan,nama,nomhp) VALUES ('$id','$nama','$hp')";
+    $hasil = mysqli_query($samb, $sql);
+    //Tambah rekod baru ke dalam table alamat
+    $sql1 = "INSERT INTO alamat (idalamat,alamat1,alamat2,bandar,poskod,negeri,idpelanggan) VALUES (NULL,'$alamat1','$alamat2','$poskod','$bandar','$negeri','$id')";
+    $hasil = mysqli_query($samb, $sql1);
     //papar mesej berjaya atau gagal simpan rekod baru
     if ($hasil) {
-        echo "<script>alert('PENDAFTARAN PELANGGAN BARU BERJAYA');
-        window.location='index2.php'</script>";
-    }else{
-        echo "<script>alert('PENDAFTARAN PELANGGAN BARU GAGAL');
-        window.location='daftar_pelanggan.php'</script>";
+      echo "<script>alert('PENDAFTARAN PELANGGAN BARU BERJAYA'); window.location='index2.php'</script>";
+    } else {
+      echo "<script>alert('PENDAFTARAN PELANGGAN BARU GAGAL'); window.location='daftar_pelanggan.php'</script>";
     }
-}
+  }
 ?>
-
-</html>
-<h2>PENDAFTARAN PELANGGAN BARU</h2>
-<body>
-<fieldset>
-<!-- Papar Borang Pendaftaran --> 
-<form method="POST"> 
-    <label>Nombor Kad Pengenalan</label><br>
-    <font size="1" color="#ff0000">*Tanpa tanda -</font><br>
-    <input type="text" name="idpelanggan"
-    placeholder="090807031234" maxlength='12'size="15"
-    onkeypress='return event.charCode >= 48 && event.charCode <= 57'
-    required autofocus><br>
-
-    <label>Nama Anda</label><br>
-    <font size="1" color="#ff0000">*Huruf besar</font><br>
-    <input type="text" name="nama" id="nama"
-    placeholder="Nama pelanggan" size="60" required ><br>
-
-    <label>Nombor Telefon</label><br>
-    <input type="text" name="nomhp" placeholder="0187654321"
-    maxlength='12'size="15"
-    onkeypress='return event.charCode >= 48 && event.charCode <= 57'
-    required autofocus><br>
-
-    <label><u>Alamat:</u></label><br>
-    <label>Alamat1</label><br>
-    <input type="text" name="alamat1" id="alamat1"
-    placeholder="Alamat1" size="60" required ><br>
-    <label>Alamat2</label><br>
-    <input type="text" name="alamat2" id="alamat2"
-    placeholder="Alamat2" size="60"><br>
-    <label>Bandar</label><br>
-    <input type="text" name="bandar" id="bandar"
-    placeholder="Bandar" size="40" required><br>
-    <label>Poskod</label><br>
-    <input type="text" name="poskod" placeholder="18000"
-    maxlength='5'size="7"
-    onkeypress='return event.charCode >= 48 && event.charCode <= 57'
-    required autofocus><br>
-    <label>Negeri</label><br>
-    <input type="text" name="negeri" id="negeri"
-    placeholder="Negeri" size="30" required ><br><br>
-<button type="submit">Daftar</button>
-<button type="reset">Reset</button><br><br>
-*Pastikan semua maklumat ditaip dengan teliti.
-</form>
-<form action="index2.php"><button type="submit">Home</button><br><br>
-</fieldset>
-</body>
+<html>
+  <h2>PENDAFTARAN PELANGGAN BARU</h2>
+  <body>
+    <fieldset>
+      <!-- Papar Borang Pendaftaran -->
+      <form method="POST">
+        <label>Nombor Kad Pengenalan</label><br>
+        <font size="1" color="#ff0000">*Tanpa tanda -</font><br>
+        <input type="text" name="idpelanggan" placeholder="090807031234" maxlength='12' size="15" onkeypress='return event.charCode >= 48 && event.charCode <= 57' required autofocus><br>
+        <label>Nama Anda</label><br>
+        <font size="1" color="#ff0000">*Huruf besar</font><br>
+        <input type="text" name="nama" id="nama" placeholder="Nama pelanggan" size="60" required><br>
+        <label>Nombor Telefon</label><br>
+        <input type="text" name="nomhp" placeholder="0187654321" maxlength='12' size="15" onkeypress='return event.charCode >= 48 && event.charCode <= 57' required autofocus><br>
+        <label><u>Alamat:</u></label><br>
+        <label>Alamat1</label><br>
+        <input type="text" name="alamat1" id="alamat1" placeholder="Alamat1" size="60" required><br>
+        <label>Alamat2</label><br>
+        <input type="text" name="alamat2" id="alamat2" placeholder="Alamat2" size="60"><br>
+        <label>Bandar</label><br>
+        <input type="text" name="bandar" id="bandar" placeholder="Bandar" size="40" required><br>
+        <label>Poskod</label><br>
+        <input type="text" name="poskod" placeholder="18000" maxlength='5' size="7" onkeypress='return event.charCode >= 48 && event.charCode <= 57' required autofocus><br>
+        <label>Negeri</label><br>
+        <input type="text" name="negeri" id="negeri" placeholder="Negeri" size="30" required><br><br>
+        <button type="submit">Daftar</button>
+        <button type="reset">Reset</button><br><br>
+        *Pastikan semua maklumat ditaip dengan teliti.
+      </form>
+      <form action="index2.php"><button type="submit">Home</button></form><br><br>
+    </fieldset>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
-//sambung ke pangkalan data
-require('config.php');
-//sambung ke fail header
-require('header.php');
-//mulakan sesi login untuk kekalkan login 
-session_start();
-//semak sama ada data dengan ID pengguna nama telah dihantar
-if (isset($_POST['idpengguna'])) {
-  //pembolehubah untuk memegang data yang dihantar
-  $user = $_POST['idpengguna'];
-  $pass = $_POST['katalaluan'];
-  //arahan sql akan dilaksanakan
-  $query = mysqli_query($samb, "SELECT * FROM penggunaWHERE IdPengguna='$user' AND KataLaluan='$pass'");
-  $row = mysqli_fetch_assoc($query);
-  if (mysqli_num_rows($query) == 0 || $row['KataLaluan'] != $pass) {
-    //papar mesej gagal login 
-    echo "<script>alert('ID Pengguna atau Katalaluan yang salah'); window.location='index.php'</script>";
-  } else {
-    $_SESSION['idpengguna'] = $row['IdPengguna'];
-    // $_SESSION['level'] = $row['status'];
-    //buka laman utama berdasarkan level login
-    header("Location: index2.php");
+  //sambung ke pangkalan data
+  require('config.php');
+  //sambung ke fail header
+  require('header.php');
+  //mulakan sesi login untuk kekalkan login 
+  session_start();
+  //semak sama ada data dengan ID pengguna nama telah dihantar
+  if (isset($_POST['idpengguna'])) {
+    //pembolehubah untuk memegang data yang dihantar
+    $user = $_POST['idpengguna'];
+    $pass = $_POST['katalaluan'];
+    //arahan sql akan dilaksanakan
+    $query = mysqli_query($samb, "SELECT * FROM pengguna WHERE IdPengguna='$user' AND KataLaluan='$pass'");
+    $row = mysqli_fetch_assoc($query);
+    if (mysqli_num_rows($query) == 0 || $row['KataLaluan'] != $pass) {
+      //papar mesej gagal login 
+      echo "<script>alert('ID Pengguna atau Katalaluan yang salah'); window.location='index.php'</script>";
+    } else {
+      $_SESSION['idpengguna'] = $row['IdPengguna'];
+      // $_SESSION['level'] = $row['status'];
+      //buka laman utama berdasarkan level login
+      header("Location: index2.php");
+    }
   }
-}
 ?>
 <html>
   <body>
