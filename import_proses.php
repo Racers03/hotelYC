b29e274c86f3a12b60a9afb98fd512c3dd683592
Cheah Zixu <cheah3838@gmail.com>
2020-10-22 18:38:20 +0800

    Fixed CSV import process

--- import_proses.php
+++ import_proses.php
@@ -1,14 +1,14 @@
 <?php
   require('config.php');
   if(isset($_POST["Import"])){
-    $filename=$_FILES["file"]["tap_name"];
+    $filename=$_FILES["file"]["tmp_name"];
   }
   if($_FILES["file"]["size"] > 0){
     $file = fopen($filename, "r");
     while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
     {
       //tambah dalam baris
-      $save = "INSERT INTO pengguna (nama_pengguna,nama.kata_laluan,status) values ('".$getData[0]."','"$getData[1]."','".$getData[2]."','".$getData[3]."')";
+      $save = "INSERT INTO pengguna values ('$getData[0]','$getData[1]','$getData[2]','$getData[3]')";
       $result = mysqli_query($samb,$save);
       if(!isset($result)){
         echo "<script>alert('Pindah naik fail CSV gagal'); window.location = 'import_pekerja.php'</script>";
