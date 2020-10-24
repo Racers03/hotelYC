a921472b5d395b8956bf757291bcda32876fe5ac
Cheah Zixu <cheah3838@gmail.com>
2020-10-24 16:55:34 +0800

    Keep darkMode state in localStorage

--- tukar.php
+++ tukar.php
@@ -6,29 +6,34 @@
     }
   </style>
   <script>
+    window.onload = function() {
+      darkModeCookie = localStorage.getItem("darkMode");
+      if (darkModeCookie == "true") {
+        toggleColor('init');
+      }
+    }
     var element = document.body;
-    function toggleColor() {
+    function toggleColor(state) {
       element.classList.toggle("dark-mode");
       if (document.getElementsByClassName('bg-light').length !== 0) {
         for (let i=0; i < document.getElementsByClassName('bg-light').length; i++) {
           document.getElementsByClassName('bg-light')[i].classList.add('bg-dark');
           document.getElementsByClassName('bg-dark')[i].classList.remove('bg-light');
+          document.getElementsByClassName('navbar-light')[i].classList.add('navbar-dark');
+          document.getElementsByClassName('navbar-dark')[i].classList.remove('navbar-light');
+        }
+        if(!state) {
+          localStorage.setItem("darkMode", true);
         }
       } else if (document.getElementsByClassName('bg-dark') !== 0) {
         for (let i=0; i < document.getElementsByClassName('bg-dark').length; i++) {
           document.getElementsByClassName('bg-dark')[i].classList.add('bg-light');
           document.getElementsByClassName('bg-light')[i].classList.remove('bg-dark');
-        }
-      }
-      if (document.getElementsByClassName('navbar-light').length !== 0) {
-        for (let i=0; i < document.getElementsByClassName('navbar-light').length; i++) {
-          document.getElementsByClassName('navbar-light')[i].classList.add('navbar-dark');
-          document.getElementsByClassName('navbar-dark')[i].classList.remove('navbar-light');
-        }
-      } else if (document.getElementsByClassName('navbar-dark') !== 0) {
-        for (let i=0; i < document.getElementsByClassName('navbar-dark').length; i++) {
           document.getElementsByClassName('navbar-dark')[i].classList.add('navbar-light');
           document.getElementsByClassName('navbar-light')[i].classList.remove('navbar-dark');
+        }
+        if(!state) {
+          localStorage.setItem("darkMode", false);
         }
       }
     }
